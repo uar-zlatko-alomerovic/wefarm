3cad6d74ea6c5978addaffddcf7c4eba02c73172
Zlatko Alomerovic <zlatko.alomerovic@gmail.com>
2018-07-20 16:56:17 +0200

    Generate the farmer scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180720145505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "farmers", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "farm"
+    t.string   "produce"
+    t.integer  "produce_price"
+    t.string   "wepay_access_token"
+    t.integer  "wepay_account_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
