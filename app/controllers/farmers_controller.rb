dd4fc64feb308af57d1e59c0ca37d2fcc4f8da5d
Zlatko Alomerovic <zlatko.alomerovic@gmail.com>
2018-07-27 13:54:20 +0200

    Make a difference between id and farmer_id for OAuth

--- app/controllers/farmers_controller.rb
+++ app/controllers/farmers_controller.rb
@@ -1,5 +1,5 @@
 class FarmersController < ApplicationController
-  before_action :set_farmer, only: [:show, :edit, :update, :destroy, :oauth]
+  before_action :set_farmer, only: [:show, :edit, :update, :destroy]
 
   def show
     @is_admin = current_user && current_user.id == @farmer.id
@@ -57,8 +57,9 @@ class FarmersController < ApplicationController
 
   # GET /farmers/oauth/1
   def oauth
+    @farmer = Farmer.find(params[:farmer_id])
     return redirect_to('/') unless params[:code]
-    redirect_uri = url_for(controller: 'farmers', action: 'oauth', id: params[:id], host: request.host_with_port)
+    redirect_uri = url_for(controller: 'farmers', action: 'oauth', farmer_id: params[:farmer_id], host: request.host_with_port)
     begin
       @farmer.request_wepay_access_token(params[:code], redirect_uri)
     rescue StandardError => e
@@ -79,6 +80,8 @@ class FarmersController < ApplicationController
   end
 
   def farmer_params
-    params.require(:farmer).permit(:name, :email, :password, :farm, :produce, :produce_price, :wepay_access_token, :wepay_account_id)
+    params.require(:farmer).permit(
+      :name, :email, :password, :farm, :produce, :produce_price, :wepay_access_token, :wepay_account_id, :farmer_id
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   resources :farmers
   get '/register', to: 'farmers#new'
-  get '/farmers/oauth(/:id)', to: 'farmers#oauth'
+  get '/farmers/oauth(/:farmer_id)', to: 'farmers#oauth'
 
   # session
   get '/login', to: 'sessions#new', as: :login
